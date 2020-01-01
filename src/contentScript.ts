3f11c1e4dc5ff44dfbedf335d37f6108a305ab34
Nobuhito Ibaraki <niba1122@gmail.com>
2020-01-02 01:33:52 +0900

    Refactor

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,10 +1,22 @@
 import { isSearch, isNextResult, isClearResult } from "./message-type"
 
 interface PageSearcher {
-  search: (query: string) => Node[]
+  search: (query: string) => void
+  nextResult: () => void
+  clear: () => void
 }
 
 function createPageSearcher(rootDOM: Node): PageSearcher {
+  let matchedSentences: Node[] = []
+  let matchedTexts: Node[] = []
+  let resultIndex = 0
+
+  function scrollToElement(element: Element, offset: number = 100) {
+    const clientRect = element.getBoundingClientRect()
+    const y = window.pageYOffset + clientRect.top + offset
+    scrollTo(0, y)
+  }
+
   function _searchRecursively(dom: Node, query: string): Node[] {
     let matchedNodes: Node[] = []
     dom.childNodes.forEach((node) => {
@@ -20,86 +32,72 @@ function createPageSearcher(rootDOM: Node): PageSearcher {
   }
 
   function search(query: string) {
-    const matchedDOMs = _searchRecursively(rootDOM, query)
-    return matchedDOMs
+    const matchedNodes = _searchRecursively(rootDOM, query)
+
+    matchedSentences.forEach((node) => {
+      const newNode = document.createTextNode(node.textContent)
+      node.parentNode.replaceChild(newNode, node)
+    })
+
+    const newNodes: Node[] = []
+    matchedNodes.forEach((node) => {
+      const text = node.nodeValue
+      const t = text.replace(new RegExp(query, 'g'), `<span class="ps-matched-text" style="background-color: #ffff00;">$&</span>`)
+
+      const newNode = document.createElement('span')
+      newNode.innerHTML = t
+      newNode.classList.add('ps-matched-sentence')
+
+      newNodes.push(newNode)
+
+      node.parentNode.replaceChild(newNode, node)
+    })
+
+    matchedSentences = newNodes
+    matchedTexts = Array.from(document.querySelectorAll('span.ps-matched-text'))
+    resultIndex = 0;
+    (matchedTexts[resultIndex] as HTMLElement).style.backgroundColor = '#ff8000'
+    scrollToElement(matchedTexts[resultIndex] as Element, -150)
+  }
+
+  function nextResult() {
+    (matchedTexts[resultIndex] as HTMLElement).style.backgroundColor = '#ffff00'
+    resultIndex++
+    if (matchedTexts.length === resultIndex) {
+      resultIndex = 0
+    }
+    (matchedTexts[resultIndex] as HTMLElement).style.backgroundColor = '#ff8000'
+    scrollToElement(matchedTexts[resultIndex] as Element, -150)
+  }
+
+  function clear() {
+    matchedSentences.forEach((node) => {
+      const newNode = document.createTextNode(node.textContent)
+      node.parentNode.replaceChild(newNode, node)
+    })
+    matchedSentences = []
+    matchedTexts = []
+    resultIndex = 0
   }
 
   return {
-    search
+    search,
+    nextResult,
+    clear
   }
 }
 
-function scrollToElement(element: Element, offset: number = 100) {
-  const clientRect = element.getBoundingClientRect()
-  const y = window.pageYOffset + clientRect.top + offset
-  scrollTo(0, y)
-}
-
-const MatchedTextClass = 'ps-matched-text'
-const HighlightedClass = 'ps-highlighted'
 
 function initialize() {
-  const styleDOM = document.createElement('style')
-  styleDOM.textContent = `
-.${MatchedTextClass} {
-  background-color: #ffff00;
-}
-.${MatchedTextClass}.${HighlightedClass} {
-  background-color: #ff8000;
-}
-`
-  document.head.appendChild(styleDOM)
-
   const pageSearcher = createPageSearcher(document.body)
-
-  let matchedSentences: Node[] = []
-  let matchedTexts: Node[] = []
-  let resultIndex = 0
 
   chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
     if (isSearch(request)) {
-      matchedSentences.forEach((node) => {
-        const newNode = document.createTextNode(node.textContent)
-        node.parentNode.replaceChild(newNode, node)
-      })
-
-      const nodes = pageSearcher.search(request.payload.query)
-
-      const newNodes: Node[] = []
-      nodes.forEach((node) => {
-        const text = node.nodeValue
-        const t = text.replace(new RegExp(request.payload.query, 'g'), `<span class="${MatchedTextClass}">$&</span>`)
-
-        const newNode = document.createElement('span')
-        newNode.innerHTML = t
-        newNode.classList.add('ps-matched-sentence')
-
-        newNodes.push(newNode)
-
-        node.parentNode.replaceChild(newNode, node)
-      })
-
-      matchedSentences = newNodes
-      matchedTexts = Array.from(document.querySelectorAll('span.ps-matched-text'))
-      resultIndex = 0;
-      (matchedTexts[resultIndex] as Element).classList.add(HighlightedClass)
-      scrollToElement(matchedTexts[resultIndex] as Element, -150)
+      pageSearcher.search(request.payload.query)
     } else if (isNextResult(request)) {
-      (matchedTexts[resultIndex] as Element).classList.remove(HighlightedClass)
-      resultIndex++
-      if (matchedTexts.length === resultIndex) {
-        resultIndex = 0
-      }
-      (matchedTexts[resultIndex] as Element).classList.add(HighlightedClass)
-      scrollToElement(matchedTexts[resultIndex] as Element, -150)
+      pageSearcher.nextResult()
     } else if (isClearResult(request)) {
-      matchedSentences.forEach((node) => {
-        const newNode = document.createTextNode(node.textContent)
-        node.parentNode.replaceChild(newNode, node)
-      })
-      matchedSentences = []
-      matchedTexts = []
-      resultIndex = 0
+      pageSearcher.clear()
     }
 
     // Send an empty response
