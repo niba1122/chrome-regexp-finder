7249a2e043639c7b107d30cb5c29e155f8f418f4
Nobuhito Ibaraki <nobuhito.ibaraki@zozo.com>
2020-01-15 19:46:24 +0900

    Add a test

--- __tests__/page-searcher-test.ts
+++ __tests__/page-searcher-test.ts
@@ -17,6 +17,7 @@ function setupEachTest() {
 <section>
   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   <p>Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris. Integer in mauris eu nibh euismod gravida. Duis ac tellus et risus vulputate vehicula. Donec lobortis risus a elit. Etiam tempor. Ut ullamcorper, ligula eu tempor congue, eros est euismod turpis, id tincidunt sapien risus a quam. Maecenas fermentum consequat mi. Donec fermentum. Pellentesque malesuada nulla a mi. Duis sapien sem, aliquet nec, commodo eget, consequat quis, neque. Aliquam faucibus, elit ut dictum aliquet, felis nisl adipiscing sapien, sed malesuada diam lacus eget erat. Cras mollis scelerisque nunc. Nullam arcu. Aliquam consequat. Curabitur augue lorem, dapibus quis, laoreet et, pretium ac, nisi. Aenean magna nisl, mollis quis, molestie eu, feugiat in, orci. In hac habitasse platea dictumst.</p>
+  <p>Rorem <span>ipsum <span>hogehoge</span></span> sit <span>amet<span></p>
 </section>
 `
   const pageSearcher = createPageSearcher(rootDOM)
@@ -155,4 +156,13 @@ test('clear result if search by empty text', (done) => {
     done()
   })
   pageSearcher.search('')
+})
+
+test('search over DOMs', (done) => {
+  pageSearcher.addChangeHighlightListener((total, current) => {
+    expect(total).toBe(2)
+    expect(current).toBe(0)
+    done()
+  })
+  pageSearcher.search('ipsum\\s\\w+\\ssit\\samet')
 })
