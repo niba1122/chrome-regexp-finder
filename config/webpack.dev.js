2183c59e26cd009cf90c5e62e4a6d125321dbd55
Nobuhito Ibaraki <nobuhito.ibaraki@zozo.com>
2020-01-20 14:27:20 +0900

    Divide webpack config & build folder

---
+++ config/webpack.dev.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const merge = require('webpack-merge');
+
+const common = require('./webpack.common.js');
+const PATHS = require('./paths');
+
+// Merge webpack configuration files
+const config = merge(common, {
+  entry: {
+    popup: PATHS.src + '/popup.ts',
+    contentScript: PATHS.src + '/contentScript.ts',
+    background: PATHS.src + '/background.ts',
+  },
+  output: {
+    path: PATHS.buildDev
+  }
+});
+
+module.exports = config;
