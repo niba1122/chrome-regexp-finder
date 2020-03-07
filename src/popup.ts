b4b4b0bc007cfb4ee118611c09bfa7abfd8768bd
Nobuhito Ibaraki <niba1122@gmail.com>
2020-03-07 10:38:50 +0900

    Refactor popup

--- src/popup.ts
+++ src/popup.ts
@@ -1,180 +1,14 @@
 'use strict';
 
-import './popup.css';
-import { Search, MessageType, NextResult, ClearResult, isChangeHighlight, PreviousResult, GetCursorSelection } from './message-type';
+import { MessageType, ClearResult } from './message-type';
+import * as ReactDOM from 'react-dom';
+import App from './popup/App';
+import * as React from 'react';
 
-const searchFormTextDOM = document.getElementById('search-form-text') as HTMLInputElement
-const searchResultTotalDOM = document.getElementById('search-result-total') as HTMLElement
-const searchResultCurrentDOM = document.getElementById('search-result-current') as HTMLElement
-const searchButtonDOM = document.getElementById('search-button') as HTMLElement
-const nextButtonDOM =  document.getElementById('forward-button') as HTMLElement
-const previousButtonDOM = document.getElementById('backward-button') as HTMLElement
-
-class QueryHistoryStorage {
-  private static STORAGE_KEY = 'query_history'
-  private static SEPARATOR = '//' // `//` cannot exist in a regular expression
-  private static MAX_COUNT = 10
-  constructor(
-    private getter: (key: string) => string | null,
-    private setter: (key: string, value: string) => void
-  ) { }
-  set(query: string) {
-    const Class = QueryHistoryStorage
-    const current = this.getter(Class.STORAGE_KEY)?.split(Class.SEPARATOR) ?? []
-    const updated = [query, ...current]
-      .filter((value, index, self) => self.indexOf(value) === index)
-      .slice(0, Class.MAX_COUNT)
-    this.setter(Class.STORAGE_KEY, updated.join(Class.SEPARATOR))
-  }
-  getAll(): string[] {
-    const Class = QueryHistoryStorage
-    return this.getter(Class.STORAGE_KEY)?.split(Class.SEPARATOR) ?? []
-  }
-}
-
-const queryHistoryStorage = new QueryHistoryStorage(
-  (key) => localStorage.getItem(key),
-  (key, value) => localStorage.setItem(key, value)
+ReactDOM.render(
+  React.createElement(App),
+  document.getElementById("app")
 )
-
-function sendSearchMessage(query: string) {
-  const message: Search = {
-    type: MessageType.Search,
-    payload: {
-      query
-    }
-  }
-
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    const tab = tabs[0]
-    if (tab.id) {
-      chrome.tabs.sendMessage(
-        tab.id,
-        message
-      );
-    }
-  });
-}
-
-function sendNextResultMessage() {
-  const message: NextResult = {
-    type: MessageType.NextResult,
-    payload: undefined
-  }
-
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    const tab = tabs[0];
-    if (tab.id) {
-      chrome.tabs.sendMessage(
-        tab.id,
-        message
-      );
-    }
-  });
-}
-
-function sendPreviousResultMessage() {
-  const message: PreviousResult = {
-    type: MessageType.PreviousResult,
-    payload: undefined
-  }
-
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    const tab = tabs[0];
-    if (tab.id) {
-      chrome.tabs.sendMessage(
-        tab.id,
-        message
-      );
-    }
-  });
-}
-
-let previousQueryInPopup = ''
-
-searchFormTextDOM.focus();
-
-searchFormTextDOM.value = queryHistoryStorage.getAll()[0] || ''
-
-chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-  const tab = tabs[0];
-  if (tab.id) {
-    const message: GetCursorSelection = {
-      type: MessageType.GetCursorSelection,
-      payload: undefined
-    }
-    chrome.tabs.sendMessage(
-      tab.id,
-      message,
-      (response) => {
-        const text: string | undefined = response.text
-        if (text) {
-          searchFormTextDOM.value = text
-        }
-      }
-    );
-  }
-});
-
-nextButtonDOM.addEventListener('click', () => {
-  const message: NextResult = {
-    type: MessageType.NextResult,
-    payload: undefined
-  }
-
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    const tab = tabs[0];
-    if (tab.id) {
-      chrome.tabs.sendMessage(
-        tab.id,
-        message
-      );
-    }
-  });
-})
-
-previousButtonDOM.addEventListener('click', () => {
-  const message: PreviousResult = {
-    type: MessageType.PreviousResult,
-    payload: undefined
-  }
-
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    const tab = tabs[0];
-    if (tab.id) {
-      chrome.tabs.sendMessage(
-        tab.id,
-        message
-      );
-    }
-  });
-})
-
-searchButtonDOM.addEventListener('click', () => {
-  const query = searchFormTextDOM.value
-  if (query === previousQueryInPopup) {
-    sendNextResultMessage()
-  } else {
-    sendSearchMessage(query)
-  }
-  previousQueryInPopup = query
-  queryHistoryStorage.set(query)
-})
-
-addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
-    const query = searchFormTextDOM.value
-    if (query === previousQueryInPopup && !event.shiftKey) {
-      sendNextResultMessage()
-    } else if (query === previousQueryInPopup && event.shiftKey) {
-      sendPreviousResultMessage()
-    } else {
-      sendSearchMessage(query)
-    }
-    previousQueryInPopup = query
-    queryHistoryStorage.set(query)
-  }
-})
 
 addEventListener('unload', (_event) => {
   const background = chrome.extension.getBackgroundPage()
@@ -194,22 +28,6 @@ addEventListener('unload', (_event) => {
     )
   })
 }, true)
-
-chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
-  if (isChangeHighlight(request)) {
-    searchResultTotalDOM.textContent = request.payload.total.toString()
-    const current = request.payload.current
-    searchResultCurrentDOM.textContent = current !== undefined ? (current + 1).toString() : '0'
-  }
-  sendResponse({})
-  return true
-})
-
-chrome.tabs.onUpdated.addListener((_tabId, _changeInfo, tab) => {
-  if (tab.status === 'complete') {
-    previousQueryInPopup = ''
-  }
-})
 
 chrome.tabs.executeScript({
   file: 'contentScript.js'
---
+++ src/popup/App.tsx
@@ -0,0 +1,128 @@
+import * as React from "react"
+import { useEffect, useState, useRef, useMemo } from "react";
+import { sendGetCursorSelectionMessage, sendNextResultMessage, sendPreviousResultMessage, sendSearchMessage, subscribeChangeHighlightMessage } from "./message-service";
+import QueryHistoryStorage from "./query-history-storage";
+
+const App: React.FC = () => {
+  const [total, setTotal] = useState(0)
+
+  const [current, setCurrent] = useState<number | undefined>()
+
+  const [query, setQuery] = useState('')
+  const queryRef = useValueRef(query)
+
+  const [previousQueryInPopup, setPreviousQueryInPopup] = useState('')
+  const previousQueryInPopupRef = useValueRef(previousQueryInPopup)
+
+  const searchFormTextDOMRef = useRef<HTMLInputElement>(null)
+
+  const queryHistoryStorage = useMemo(() => {
+    return new QueryHistoryStorage(
+      (key) => localStorage.getItem(key),
+      (key, value) => localStorage.setItem(key, value)
+    )
+  }, [])
+
+  const handleChangeSearchText: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setQuery(event.target.value)
+  }
+
+  const handleClickSearchButton = () => {
+    if (query === previousQueryInPopup) {
+      sendNextResultMessage()
+    } else {
+      sendSearchMessage(query)
+    }
+    setPreviousQueryInPopup(query)
+    queryHistoryStorage.set(query)
+  }
+
+  const handleClickNextButton = () => {
+    sendNextResultMessage()
+  }
+
+  const handleClickPreviousButton = () => {
+    sendPreviousResultMessage()
+  }
+
+  useEffect(() => {
+    const searchFormTextDOM = searchFormTextDOMRef.current
+    searchFormTextDOM && searchFormTextDOM.focus()
+
+    const previousQuery = queryHistoryStorage.getAll()[0]
+    previousQuery && setQuery(previousQuery)
+
+    sendGetCursorSelectionMessage((text) => {
+      if (text) {
+        setQuery(text)
+      }
+    })
+
+    addEventListener('keydown', (event) => {
+      const query = queryRef.current
+      const previousQueryInPopup = previousQueryInPopupRef.current
+      if (event.key === 'Enter') {
+        if (query === previousQueryInPopup && !event.shiftKey) {
+          sendNextResultMessage()
+        } else if (query === previousQueryInPopup && event.shiftKey) {
+          sendPreviousResultMessage()
+        } else {
+          sendSearchMessage(query)
+        }
+        setPreviousQueryInPopup(query)
+        queryHistoryStorage.set(query)
+      }
+    })
+
+    subscribeChangeHighlightMessage((request) => {
+      setTotal(request.payload.total)
+      setCurrent(request.payload.current)
+    })
+
+    chrome.tabs.onUpdated.addListener((_tabId, _changeInfo, tab) => {
+      if (tab.status === 'complete') {
+        setPreviousQueryInPopup('')
+      }
+    })
+  }, [])
+
+  return (
+    <div className="search-form">
+      <div className="search-form__text">
+        <span className="query-text">
+          <input className="query-text__input" type="text" value={query} onChange={handleChangeSearchText} placeholder="[a-z]+.*\w" ref={searchFormTextDOMRef} />
+        </span>
+      </div>
+      <div className="search-form__count">
+        <span>{current !== undefined ? (current + 1) : '0'}</span>
+        &nbsp;/&nbsp;
+        <span>{total}</span>
+      </div>
+      <div className="search-form__search-button">
+        <span className="button" onClick={handleClickSearchButton}>
+          <img className="button__image" src="images/search.svg" alt="search" />
+        </span>
+      </div>
+      <div className="search-form__backward-button" onClick={handleClickPreviousButton}>
+        <span className="button">
+          <img className="button__image" src="images/backward.svg" alt="backward" />
+        </span>
+      </div>
+      <div className="search-form__forward-button" onClick={handleClickNextButton}>
+        <span className="button">
+          <img className="button__image" src="images/forward.svg" alt="forward" />
+        </span>
+      </div>
+    </div>
+  )
+}
+
+function useValueRef<T>(value: T) {
+  const ref = useRef(value)
+  useEffect(() => {
+    ref.current = value
+  }, [value])
+  return ref
+}
+
+export default App
